671ec77a78866f499ddadcef3a2bdc5f7d4f1655
Ammar-1993 <ammaralnggar@gmail.com>
2026-07-24 05:37:06 +0300

    chore: update next.config.mjs configuration settings

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -49,10 +49,6 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "freelance-jw",
-
-  project: "javascript-nextjs",
-
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
